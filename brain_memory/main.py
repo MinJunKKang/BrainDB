4b7123dc1f74f7ed17e532b28024f187863c9d2e
MinJunKKang <kmg4613@naver.com>
2025-03-25 02:51:48 +0900

    GUI 기본 구성성

--- brain_memory/main.py
+++ brain_memory/main.py
@@ -1,5 +1,10 @@
-from utils.init_memory import init_project
+import tkinter as tk
+from gui.memory_gui import MemoryGUI
+#from utils.init_memory import init_project
 
 if __name__ == "__main__":
-    print("프로젝트 초기화 중...")
-    init_project()
+    #print("프로젝트 초기화 중...")
+    #init_project()
+    root = tk.Tk()
+    app = MemoryGUI(root)
+    root.mainloop()
